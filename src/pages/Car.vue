cef08958ffb4a957f4096a62784f2b8e83d63d2a
rdv-omsk <rdv-omsk@mail.ru>
2022-05-21 15:30:50 +0300

    6-9

--- src/pages/Car.vue
+++ src/pages/Car.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Car id {{ id }}</h1>
 
-
+    <button class="btn btn-sm btn-info" @click="goBackToCars">Back</button>
   </div>
 </template>
 
@@ -12,6 +12,11 @@ export default {
     return {
       // id: this.$router.currentRoute.params['id']
       id: this.$route.params['id']
+    }
+  },
+  methods: {
+    goBackToCars () {
+      this.$router.push('/cars')
     }
   },
   watch: {
